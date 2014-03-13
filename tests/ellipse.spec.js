ceeded98bddde86c78b91de224c0f971ebabe4fe
Ryan Zec <code@ryanzec.com>
2014-03-13 05:48:13 -0400

    coding standards update

--- tests/ellipse.spec.js
+++ tests/ellipse.spec.js
@@ -9,18 +9,18 @@ describe('Ellipse', function(){
   }));
 
   it('should use default values', function() {
-    expect(nagEllipse(ellipseText)).toBe('thisi...oint.txt');
+    expect(nagEllipse(ellipseText)).to.equal('thisi...oint.txt');
   });
 
   it('should not ellipse with custom min length', function() {
-    expect(nagEllipse(ellipseText, 10000)).toBe(ellipseText);
+    expect(nagEllipse(ellipseText, 10000)).to.equal(ellipseText);
   });
 
   it('should show the first 10 characters', function() {
-    expect(nagEllipse(ellipseText, 20, 10)).toBe('thisisaver...oint.txt');
+    expect(nagEllipse(ellipseText, 20, 10)).to.equal('thisisaver...oint.txt');
   });
 
   it('should show the first 10 and last 20 characters', function() {
-    expect(nagEllipse(ellipseText, 50, 10, 20)).toBe('thisisaver...ipsedatsomepoint.txt');
+    expect(nagEllipse(ellipseText, 50, 10, 20)).to.equal('thisisaver...ipsedatsomepoint.txt');
   });
 });
